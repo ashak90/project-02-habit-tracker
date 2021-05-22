740e3ace9d5ad8ef0f5570cf1ce1c88e96af86b3
mythosmystery <axiysfire@gmail.com>
2021-05-22 12:20:01 -0500

    added handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.send("<h1>Hello World!</h1>")
-});
+router.use('/', homeRoutes);
 
 module.exports = router;
