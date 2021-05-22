34fde0bf9478231983c604fe0929887c6667cfd5
mythosmystery <axiysfire@gmail.com>
2021-05-22 12:42:36 -0500

    added basic handlebars template for testing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 //the /api/users endpoint
 
 router.get('/', (req, res) => {
-    res.send('<h1> Hello Users </h1>');
+    res.send('<h1> Users </h1>');
 });
 
 module.exports = router;
