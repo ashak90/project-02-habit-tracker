a63baa2d0bb2e0aa066a94ee07944833042d8da5
mythosmystery <axiysfire@gmail.com>
2021-05-22 12:08:20 -0500

    added users api route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const habitRoutes = require('./habit-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/habits', habitRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
