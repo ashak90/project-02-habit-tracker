1d72d61e8bcb52a264a11d7d1a8522efefb5c575
Erica Roquemore <enroquemore@gmail.com>
2021-05-24 23:19:18 -0400

    added to models

--- models/User.js
+++ models/User.js
@@ -1,17 +1,54 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 // create User model
 class User extends Model {}
 
 // create fields/columns for User model
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-    }
-)
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    user_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+
+    user_age: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    undersocred: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
