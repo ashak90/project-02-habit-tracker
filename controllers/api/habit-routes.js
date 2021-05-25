f6cc8424831bf20d68ba27a24b10ad96c5a7db29
mythosmystery <axiysfire@gmail.com>
2021-05-25 18:15:56 -0500

    added habit routes

--- controllers/api/habit-routes.js
+++ controllers/api/habit-routes.js
@@ -15,4 +15,30 @@ router.get('/', withAuth, async (req, res) => {
     }
 });
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newHabit = await Habit.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newHabit);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const habitData = await Habit.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+        habitData ? res.status(200).json(habitData) : res.status(404).json({ message: 'No habit with this ID!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
