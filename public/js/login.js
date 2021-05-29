4b3f2fe4c187d85a365ced43e0b85d5ee768e43e
Erica Roquemore <enroquemore@gmail.com>
2021-05-28 22:55:49 -0400

    changed colors and fixed typos

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+// const cancelBtn2 = document.getElementById('cancelBtn2');
+
+
+
+
+// cancelBtn2.addEventListener('click', function () {
+//     event.preventDefault();
+//     console.log("You hit the cancel button")
+//     window.location.href = "/"
+//   })
