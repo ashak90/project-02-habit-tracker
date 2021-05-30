cf2f314a769315de4d100db662570110d0f0d504
Erica Roquemore <enroquemore@gmail.com>
2021-05-29 20:57:09 -0400

    set current date in habit.js

--- public/js/habit.js
+++ public/js/habit.js
@@ -4,3 +4,7 @@ const addNewBtn = document.getElementById("addNewBtn")
 addNewBtn.addEventListener("click", function () {
     window.location.href = "/addHabit"
 });
+
+// Current Date
+let date = new Date();
+document.getElementById('todayDate').innerHTML = date.toDateString();
