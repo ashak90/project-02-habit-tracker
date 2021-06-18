d8845914cd20e136a0ec3feb429095d18fb9e203
Asha Kumar <ashakumar90@gmail.com>
2021-06-17 20:22:56 -0400

    added compression

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ const routes = require('./controllers');
 const sequelize = require("./config/connection");
 const exphbs = require('express-handlebars');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const compression = require('compression');
 
 const app = express();
+app.use(compression())
+// function shouldCompress (req, res) {
+//     if (req.headers['x-no-compression']) {
+//         // don't compress responses with this request header
+//         return false
+//     }
+
+//     // fallback to standard filter function
+//     return compression.filter(req, res)
+// }
+
+
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
